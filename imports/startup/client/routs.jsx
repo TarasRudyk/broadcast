a3c17a233477fc9a41e11bc64551d7997c869f74
TarasRudyk <rudyktaras@gmail.com>
2016-09-08 12:01:38 +0300

    routing setup

--- imports/startup/client/routs.jsx
+++ imports/startup/client/routs.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import {mount} from 'react-mounter';
 
-import {mainLayout} from '../../ui/layouts/mainLayout'
-import App from '../../ui/App.jsx'
+import {mainLayout} from '../../ui/layouts/mainLayout';
+import Broad from '../../ui/pages/broadcast';
+import Profile from '../../ui/pages/profile';
+import About from '../../ui/pages/about';
 
 FlowRouter.route('/', {
 	action() {
 		mount(mainLayout, {
-			content: (<App />)
+			content: (<Broad />)
+		})
+	}
+});
+
+FlowRouter.route('/profile', {
+	action() {
+		mount(mainLayout, {
+			content: (<Profile />)
+		})
+	}
+});
+
+FlowRouter.route('/about', {
+	action() {
+		mount(mainLayout, {
+			content: (<About />)
 		})
 	}
 });
--- imports/ui/layouts/mainLayout.jsx
+++ imports/ui/layouts/mainLayout.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 export const mainLayout = ({content}) => (
-	<div>
+	<div className="broadcast">
+	<h2>Test</h2>
+	<nav>
+		<a href="/">Broadcast</a>
+		<a href="/profile">Profile</a>
+		<a href="/about">About owner</a>
+	</nav>
+	<main>
 	 {content}
+	</main>
   	</div>
 )
